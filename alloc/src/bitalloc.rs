265d2b361bf32a49aaa65e0a8b1b720f9ad009c6
John-Nagle <nagle@sitetruth.com>
2024-11-26 19:26:36 -0800

    Minor cleanup

--- alloc/src/bitalloc.rs
+++ alloc/src/bitalloc.rs
@@ -8,7 +8,7 @@
 use anyhow::{anyhow, Error};
 use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 
-/// Our bitmap is an array of these. u64 if necessary, u128 if atomics that large are widely available.
+/// Our bitmap is an array of these. u64 today, u128 when atomics that large are widely available.
 type WordType = u64;
 type AtomicWordType = AtomicU64;
 const WORDSIZE: usize = WordType::BITS as usize;
@@ -33,7 +33,7 @@ impl BitAlloc {
             b,
         }
     }
-    
+
     /// Length, in bits
     pub fn len(&self) -> usize {
         self.b.len() * WORDSIZE
@@ -56,16 +56,16 @@ impl BitAlloc {
             //  Retry loop for atomic CAS
             loop {
                 let val = self.b[word].load(Ordering::SeqCst); // get word
-                let newval = val & !(1<<bit);       // clear bit
+                let newval = val & !(1 << bit); // clear bit
                 let swap_result =
-                        self.b[word].compare_exchange(val, newval, Ordering::SeqCst, Ordering::Relaxed);
+                    self.b[word].compare_exchange(val, newval, Ordering::SeqCst, Ordering::Relaxed);
                 if swap_result.is_ok() {
                     break;
                 }
                 println!("Race condition in clear_bit, retrying"); // ***TEMP***
             }
             //  Updated successfuly. Update start position for next search if this is the new min
-            let _ = self.search_pos.fetch_min(word,  Ordering::Relaxed);
+            let _ = self.search_pos.fetch_min(word, Ordering::Relaxed);
             Ok(())
         } else {
             Err(anyhow!("Bitalloc clear_bit index out of range."))
@@ -86,7 +86,7 @@ impl BitAlloc {
                 //  There may be an open slot in this word.
                 //  But we have to test that with an atomic operation.
                 let bit = (!val).trailing_zeros(); // find first zero bit.
-                println!("val: {:#x} bit: {}", val, bit);// ***TEMP***
+                                                   /////println!("val: {:#x} bit: {}", val, bit);// ***TEMP***
                 let newval = val | (1 << bit); // new value for bitmap word
 
                 //  Now try to insert that into the map with a compare and swap.
@@ -95,7 +95,12 @@ impl BitAlloc {
                     self.b[word].compare_exchange(val, newval, Ordering::SeqCst, Ordering::Relaxed);
                 if let Ok(_) = swap_result {
                     //  Update search start position to try from here next time.
-                    let pos_result = self.search_pos.compare_exchange(word, start_pos, Ordering::Relaxed, Ordering::Relaxed);
+                    let pos_result = self.search_pos.compare_exchange(
+                        start_pos,
+                        word,
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    );
                     if let Err(_) = pos_result {
                         //  This is just an unsucessful optimization
                         println!("Race condition in alloc_bit pos update, harmless");
@@ -116,25 +121,15 @@ impl BitAlloc {
         (index / WORDSIZE, index % WORDSIZE)
     }
 }
-/*
-/// Find first one bit. Should be an intrinsic.
-fn find_first_one_bit(v: WordType) -> Option<u32> {
-    for bit in 0..usize::BITS {
-        if v & (1 << bit) == 1 {
-            return Some(bit);
-        }
-    }
-    None // no find
-}
-*/
+
 #[test]
 /// Basic test. Does this work at all?
 fn test_bitalloc_basics() {
     /// Build up a list of bits
     fn bit_list(item: &BitAlloc) -> Vec<usize> {
         (0..item.len())
-        .filter_map(|n| if item.get_bit(n) { Some(n) } else { None })
-        .collect()
+            .filter_map(|n| if item.get_bit(n) { Some(n) } else { None })
+            .collect()
     }
     //  Try some basic operations
     let bit_alloc = BitAlloc::new(1000);
@@ -145,9 +140,10 @@ fn test_bitalloc_basics() {
     let v2 = bit_alloc.alloc_bit().unwrap();
     assert_eq!(v2, 2);
     assert_eq!(bit_list(&bit_alloc), [0, 1, 2]);
-    bit_alloc.clear_bit(v1);
+    bit_alloc.clear_bit(v1).unwrap();
     assert_eq!(bit_list(&bit_alloc), [0, 2]);
-    for i in 0..500 {
+    /// Allocate 500 multiple words and see if that works.
+    for _ in 0..500 {
         let _ = bit_alloc.alloc_bit().unwrap();
     }
     //  Construct expected result.
@@ -155,5 +151,4 @@ fn test_bitalloc_basics() {
     let vlist1: Vec<usize> = (3..502).map(|n: usize| n).collect();
     vlist.extend(vlist1);
     assert_eq!(bit_list(&bit_alloc), vlist);
-    assert!(false);
 }
