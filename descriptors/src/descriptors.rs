65bd43d5605befc54abc72e2a21e563703df50b2
John-Nagle <nagle@sitetruth.com>
2024-12-10 19:46:59 -0800

    More defs re descriptors

--- descriptors/src/descriptors.rs
+++ descriptors/src/descriptors.rs
@@ -13,10 +13,75 @@ use anyhow::{Error};
 use ash::{vk};
 use vk::Handle;
 
+#[repr(u32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+/// The types of descriptor tables. These live in the GPU.
+enum DescriptorTableType {
+    StorageBuffer = 0,
+    SampledImage = 1,
+    StorageImage = 2,
+}
+
+impl DescriptorTableType {
+    /// Return all the types
+    fn all_types() -> impl Iterator<Item = Self> {
+        [Self::StorageBuffer, Self::SampledImage, Self::StorageImage].into_iter()
+    }
+
+    fn set_index(self) -> u32 {
+        self as u32
+    }
+
+    fn from_set_index(set_index: u32) -> Self {
+        match set_index {
+            0 => Self::StorageBuffer,
+            1 => Self::SampledImage,
+            _ => panic!("invalid set index"),
+        }
+    }
+
+    /// Local name to Vulkann ame.
+    fn to_vk(self) -> vk::DescriptorType {
+        match self {
+            DescriptorTableType::StorageBuffer => vk::DescriptorType::STORAGE_BUFFER,
+            DescriptorTableType::SampledImage => vk::DescriptorType::SAMPLED_IMAGE,
+            DescriptorTableType::StorageImage => vk::DescriptorType::STORAGE_IMAGE,
+        }
+    }
+
+    /// Display name
+    fn name(self) -> &'static str {
+        match self {
+            DescriptorTableType::StorageBuffer => "storage_buffer",
+            DescriptorTableType::SampledImage => "sampled_image",
+            DescriptorTableType::StorageImage => "storage_image",
+        }
+    }
+
+    fn max_count(self, gpu: &GpuInfo) -> u32 {
+        let props = &gpu.properties.properties12;
+        match self {
+            DescriptorTableType::StorageBuffer => u32::min(
+                props.max_descriptor_set_update_after_bind_storage_buffers,
+                props.max_per_stage_descriptor_update_after_bind_storage_buffers,
+            ),
+            DescriptorTableType::SampledImage => u32::min(
+                props.max_descriptor_set_update_after_bind_sampled_images,
+                props.max_per_stage_descriptor_update_after_bind_sampled_images,
+            ),
+            DescriptorTableType::StorageImage => u32::min(
+                props.max_descriptor_set_update_after_bind_storage_images,
+                props.max_per_stage_descriptor_update_after_bind_storage_images,
+            ),
+        }
+    }
+}
+
+
 /// Descriptors
 pub struct Descriptors {
     /// The descriptor set
-    descriptor_sets: DescriptorSets
+    descriptor_sets: Vec<vk::DescriptorSet>,
 }
 
 impl Descriptors {
@@ -72,7 +137,7 @@ impl Descriptors {
             set_debug_name(vk::DescriptorSet::TYPE, descriptor_set.as_raw(), names[i]);
         }
 
-        log::info!("created device");
+        log::info!("created descriptor table");
         
         Ok(Self {
             descriptor_sets
